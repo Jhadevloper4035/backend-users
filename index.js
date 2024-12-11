8117977d0fdf516dc634eef395e0e9aaae16dbd5
Jhadevloper4035 <140846093+Jhadevloper4035@users.noreply.github.com>
2024-12-12 01:43:55 +0530

    feat: Adding api to get user and user by id

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require("cors")
+const PORT = 3000
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+app.listen(PORT,() => {
+    console.log(`server is running on ${PORT}`)
+})
